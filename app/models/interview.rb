59e0edf62460cc7cf8cb49fada8251d05ba29d5b
snmalineni <snmalineni@gmail.com>
2020-08-25 19:38:33 -0700

    stage and timeslot changes f20

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -16,7 +16,7 @@ class Interview < ActiveRecord::Base
   default_scope { order "timeslot" }
 
   # Interview capacity by stage
-  INTERVIEW_CAPACITY = { 0 => 1, 1 => 1, 1 => 1}
+  INTERVIEW_CAPACITY = { 0 => 1, 1 => 1, 2 => 1}
 
   def self.available
     Interview.all.select(&:available?)
